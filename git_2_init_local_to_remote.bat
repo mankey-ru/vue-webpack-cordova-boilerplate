68f07e9190daca72be535c12a6baab499f11ed11
mankey-ru <mankey-ru@yandex.ru>
2017-02-25 09:29:14 +0300

    1

--- git_2_init_local_to_remote.bat
+++
@@ -1,6 +0,0 @@
-echo "# vue-webpack-cordova-boilerplate" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/mankey-ru/vue-webpack-cordova-boilerplate.git
-git push -u origin master
